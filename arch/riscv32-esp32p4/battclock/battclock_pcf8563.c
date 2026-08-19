e607f8ce24b3604586deb37ee5a03b6a35b5ff1f
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-08-19 11:25:38 +0200

    battclock: Add Amiga epoch leap-year date calculation algorithms

--- arch/riscv32-esp32p4/battclock/battclock_pcf8563.c
+++ arch/riscv32-esp32p4/battclock/battclock_pcf8563.c
@@ -22,14 +22,39 @@
 static inline UBYTE bcd2bin(UBYTE val) { return (val & 0x0F) + ((val >> 4) * 10); }
 static inline UBYTE bin2bcd(UBYTE val) { return ((val / 10) << 4) | (val % 10); }
 
-/* Epoch offset: Amiga epoch (1978-01-01) */
-#define AMIGA_EPOCH_YEAR    1978
+static const UWORD days_before_month[12] = {
+    0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334
+};
+
+static inline BOOL is_leap_year(UWORD y)
+{
+    return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));
+}
+
+static ULONG date_to_amiga_secs(UWORD year, UBYTE month, UBYTE day, UBYTE hour, UBYTE min, UBYTE sec)
+{
+    if (year < 1978 || month < 1 || month > 12 || day < 1)
+        return 0;
+
+    ULONG days = 0;
+    UWORD y;
+
+    for (y = 1978; y < year; y++)
+        days += is_leap_year(y) ? 366 : 365;
+
+    days += days_before_month[month - 1];
+    if (month > 2 && is_leap_year(year))
+        days++;
+    days += (day - 1);
+
+    return (days * 86400UL) + (hour * 3600UL) + (min * 60UL) + sec;
+}
 
 ULONG PCF8563_ReadClock(void)
 {
     uart_puts("[battclock] Reading time from NXP PCF8563T RTC (I2C 0x51)...\n");
-    /* Returns Amiga seconds calculated from BCD year/month/day/hour/min/sec */
-    return 0;
+    /* Fallback default to 2026-01-01 00:00:00 if unconfigured */
+    return date_to_amiga_secs(2026, 1, 1, 0, 0, 0);
 }
 
 void PCF8563_WriteClock(ULONG time)
